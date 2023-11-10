315ab5c4560f9161435864f90814b64ccefef875
Renato <rcoelhomf@gmail.com>
2023-11-10 17:37:26 -0300

    feat: models

--- bedrooms/models.py
+++ bedrooms/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class RoomStatus(models.TextChoices):
+    LIMPO = 'limpo'
+    OCUPADO = 'ocupado'
+    MANUTENCAO = 'manutenção'
+    DISPONIVEL = 'disponível'
+
+
+class Bedroom(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    status = models.CharField(max_length=10, choices=RoomStatus.choices, default=RoomStatus.DISPONIVEL)
+    room_type = models.CharField(max_length=20)
+    price = models.IntegerField()
+    image = models.TextField()
--- promotions/models.py
+++ promotions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Promotion(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=30)
+    discount_amount = models.IntegerField()
+    description = models.CharField(max_length=140)
