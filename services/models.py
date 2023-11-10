b5c959df597bc73222c4f8016a64668d76ccab74
DaviMBarros <davimatiasdebarros@gmail.com>
2023-11-10 18:08:09 -0300

    feat: alter name models

--- services/models.py
+++ services/models.py
@@ -2,6 +2,6 @@ from django.db import models
 import uuid
 
 
-class Payments(models.Model):
+class Service(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     service = models.CharField(max_length=200)
