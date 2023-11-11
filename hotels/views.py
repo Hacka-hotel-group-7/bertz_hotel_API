96c2a7942dbf480897266d33b682863bef6a1de9
DaviMBarros <davimatiasdebarros@gmail.com>
2023-11-11 15:33:44 -0300

    feat: create HotelView and HotelDatailView

--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from .models import Hotel
+from .serializers import HotelSerializer
 
-# Create your views here.
+
+class HotelView(ListCreateAPIView):
+
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+
+
+class HotelDetailView(RetrieveUpdateDestroyAPIView):
+
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+    lookup_url_kwarg = "hotel_id"
