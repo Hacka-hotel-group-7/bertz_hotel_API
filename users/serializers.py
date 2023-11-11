612193960f111d4a28b657c9f0a3538ef1e40dfb
Renato <rcoelhomf@gmail.com>
2023-11-11 18:14:14 -0300

    feat: custom token

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomJWTSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["role"] = user.role
+
+        return token
 
 
 class UserSerializer(serializers.ModelSerializer):
--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, CreateAPIView, ListCreateAPIView
 from .models import User
 from .serializers import UserSerializer
+from .serializers import CustomJWTSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class LoginJWTView(TokenObtainPairView):
+    serializer_class = CustomJWTSerializer
 
 
 class StaffView(ListCreateAPIView):
