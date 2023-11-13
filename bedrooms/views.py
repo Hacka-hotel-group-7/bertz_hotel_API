f29fd74691a38f84f263e31455333b3ffdc1e137
DaviMBarros <davimatiasdebarros@gmail.com>
2023-11-13 12:25:48 -0300

    create views: bedrooms, reviews and promotions

--- bedrooms/views.py
+++ bedrooms/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from .models import Bedroom
+from .serializers import BedroomSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class BedroomView(CreateAPIView):
+
+    queryset = Bedroom.objects.all()
+    serializer_class = BedroomSerializer
+    lookup_url_kwarg = "hotel_id"
+
+
+class BedroomDetailView(UpdateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = []
+
+    queryset = Bedroom.objects.all()
+    serializer_class = BedroomSerializer
+    lookup_url_kwarg = "bedroom_id"
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+
+class ReviewView(ListCreateAPIView):
+
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_url_kwarg = "hotel_id"
+
+
+class ReviewDetailView(RetrieveUpdateDestroyAPIView):
+
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_url_kwarg = "review_id"
