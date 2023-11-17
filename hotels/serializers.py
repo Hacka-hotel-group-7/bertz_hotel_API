eaaaae2289a12100ed85844d89ce3a2420e6b459
Renato <rcoelhomf@gmail.com>
2023-11-17 12:22:13 -0300

    fix: service route

--- hotels/serializers.py
+++ hotels/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Hotel
 from hotel_images.serializers import HotelImageSerializer
 from bedrooms.serializers import BedroomSerializer
+from services.serializers import ServiceSerializer
 
 
 class HotelSerializer(serializers.ModelSerializer):
     images = HotelImageSerializer(read_only=True, many=True)
     bedrooms = BedroomSerializer(read_only=True, many=True)
+    services = ServiceSerializer(read_only=True, many=True)
 
     class Meta:
         model = Hotel
         fields = "__all__"
-        read_only_fields = ['services', 'reviews', 'promotions']
+        read_only_fields = ['reviews', 'promotions']
--- services/views.py
+++ services/views.py
@@ -1,9 +1,13 @@
 from rest_framework.generics import ListCreateAPIView
 from .models import Service
 from .serializers import ServiceSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .permissions import HaveStaffPermission
 
 
 class ServiceView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [HaveStaffPermission]
 
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
